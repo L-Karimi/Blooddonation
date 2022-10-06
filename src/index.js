8da902e42ba899b5c37270b3b62c1d14b1ffc325
L-Karimi <luciahkei101@gmail.com>
2022-10-06 23:13:38 +0300

    modified files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ProSidebarProvider } from 'react-pro-sidebar';
 
 ReactDOM.render(
   <React.StrictMode>
+    <ProSidebarProvider>
     <App />
+    </ProSidebarProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
